4e681042bcf3e2be5c7542a9eca0842661509583
Minglena <3322455670@qq.com>
2019-07-30 18:37:34 +0800

    梯度幅值与canny算子比较，相加在道路阈值分割中。

--- test_Threshold_segmentation.py
+++ test_Threshold_segmentation.py
@@ -1,15 +1,30 @@
+
 import cv2
 import numpy as np
 
+def con_matrix(new_image,weight,height):#拼接矩阵函数，拼接三行三列
+    left_matrix = matrix(1, height - 1)
+    new_image = np.hstack((left_matrix, new_image))
+    new_image = np.hstack((left_matrix, new_image))
+    new_image = np.hstack((new_image, left_matrix))
+    upper_matrix = matrix(weight + 2, 1)
+    new_image = np.vstack((upper_matrix, new_image))
+    new_image = np.vstack((upper_matrix, new_image))
+    new_image = np.vstack((new_image, upper_matrix))
+    return new_image
+def matrix(m,n):#创建矩阵函数
+    matrix = [None] * n
+    for i in range(len(matrix)):
+        matrix[i] = [0] * m
+    matrix=np.array(matrix)
+    matrix=matrix.astype(np.uint8)
+    return matrix
+    # print(matrix.dtype)
 def gradient(image):#梯度幅值
     weight,height=image.shape
     new_image=[]
     for i in range(2,weight-1):
         he=[]
-        # if i==2:
-        #     for j in range(1,height):
-        #         he.append(0)
-        #
         for j in range(2,height-1):
             fx=(int(image[i,j+1])-int(image[i,j-1]))/2
             fy=(int(image[i+1,j])-int(image[i-1,j]))/2
@@ -18,37 +33,46 @@ def gradient(image):#梯度幅值
             hei=np.array(he)
         new_image.append(hei)
     new_img=np.array(new_image)
-    return new_img.astype(np.uint8)
-
+    return new_img.astype(np.uint8),height-2,weight-2
 
 src=cv2.imread("C:\\Users\\Minglena\\Pictures\\test\\timg9.jpg")
 src=cv2.resize(src,(640,480))
-test_gray=cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)
+test_gray=cv2.cvtColor(src,cv2.COLOR_BGR2GRAY)#RGB图像转换为HSV图像
 test_src=cv2.cvtColor(src,cv2.COLOR_BGR2HSV)
 H,S,V=cv2.split(test_src)
-cv2.imshow("H",H)
-cv2.imshow("S",S)
-cv2.imshow("V",V)
-# print(V)
 test_src=cv2.bilateralFilter(test_src,0,30,3)
-test_ret1,test_thresh1=cv2.threshold(V,0,255,cv2.THRESH_OTSU)
-cv2.imshow("test_thresh1",test_thresh1)
-t1=cv2.getTickCount()
-new_image=gradient(V)
-t2=cv2.getTickCount()
-t=(t2-t1)/cv2.getTickFrequency()
-print(t)
-cv2.imshow("new_image",new_image)
+test_ret1,test_thresh1=cv2.threshold(V,0,255,cv2.THRESH_OTSU)#道路阈值分割
+
+"""
+梯度幅值算法，与下面的canny算子只能同时选一个
+"""
+# test_kernel1 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))  # 卷积核
+# test_kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))  # 卷积核
+# test_kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # 卷积核
+# t1=cv2.getTickCount()
+# new_image,weight,height=gradient(V)
+# new_image=con_matrix(new_image,weight,height)
+# t2 = cv2.getTickCount()
+# t = (t2 - t1) / cv2.getTickFrequency()
+# print(t)
+# cv2.imshow("new_image",new_image)
+
+"""
+canny算子
+"""
+test_kernel1 = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))  # 卷积核
+test_kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9))  # 卷积核
+test_kernel3 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # 卷积核
+test_canny=cv2.Canny(test_thresh1,10,150)
+new_image = cv2.dilate(test_canny,test_kernel3)  # 膨胀操作
+cv2.imshow("canny",new_image)
+
 test_ret2,test_thresh2=cv2.threshold(new_image,0,255,cv2.THRESH_OTSU)
 cv2.imshow("test_thresh2",test_thresh2)
-test_canny=cv2.Canny(test_thresh1,10,150)
-cv2.imshow("canny",test_canny)
-# add=cv2.add(thresh1,thresh2)
-# # cv2.imshow("add",add)
-test_kernel1 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # 卷积核
-test_thresh = cv2.dilate(test_thresh1,test_kernel1)  # 膨胀操作
+add=cv2.add(test_thresh1,test_thresh2)
+# cv2.imshow("add",add)
+test_add = cv2.dilate(add,test_kernel1)  # 膨胀操作
 #面积约束算法
-test_kernel2 = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))  # 卷积核
-test_erode=cv2.erode(test_thresh,test_kernel2)
+test_erode=cv2.erode(test_add,test_kernel2)
 cv2.imshow("erode",test_erode)
 cv2.waitKey()
